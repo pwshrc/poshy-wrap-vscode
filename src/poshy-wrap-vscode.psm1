554f9f68d165041dba5df47434e2135f39a8b79f
Bruce Markham <219281+brucificus@users.noreply.github.com>
2023-08-25 21:56:38 -0400

    ✨ Updates Module Implementation

--- src/poshy-wrap-vscode.psm1
+++ src/poshy-wrap-vscode.psm1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
 
-if ((-not $Env:VSCODE) -and (-not (Test-Command code))) {
+if ((-not $Env:VSCODE) -and (-not (Test-Command code)) -and (-not $Global:PWSHRC_FORCE_MODULES_EXPORT_UNSUPPORTED)) {
     return
 }
 
